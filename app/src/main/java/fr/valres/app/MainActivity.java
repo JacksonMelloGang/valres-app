e3ba1844fb225ed9f39ed354d7ce84ef1915eb4b
Yulkoga <yann7745@gmail.com>
2023-03-16 11:33:29 +0100

    changeed

--- app/src/main/java/fr/valres/app/MainActivity.java
+++ app/src/main/java/fr/valres/app/MainActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static Connection connection = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    MainActivity.connection = DriverManager.getConnection("jdbc:mysql://localhost/valres", "root", "");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    System.exit(0);
+                }
+            }
+        });
+
+        thread.start();
+
     }
 }
